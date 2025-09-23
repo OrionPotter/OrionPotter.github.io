3aeeba306b1e87fd283b8b7705ad3fb367314137
liuzhi <1759586066@qq.com>
2025-09-23 22:05:37 +0800

    auto submit

--- submit.sh
+++ submit.sh
@@ -3,7 +3,8 @@ hexo clean
 hexo g
 hexo d
 echo "save theme config"
-cp -a  themes/next/_config.yml source/
+# 备份当前主题配置
+cp -a themes/next/_config.yml source/_config.next.yml
 git add .
 git commit -m "auto submit"
 git push origin hexo
